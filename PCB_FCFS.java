3661f178c32a7ca6c84ba729c835543e8e19348b
feelimelek <80131744+feelimelek@users.noreply.github.com>
2021-10-28 01:38:43 -0300

    Add files via upload

---
+++ PCB_FCFS.java
@@ -0,0 +1,22 @@
+package kernel.PCB;
+
+import operacoes.Operacao;
+
+public class PCB_FCFS extends PCB{
+
+
+    public PCB_FCFS(Operacao[] codigo) {
+        super(codigo);
+    }
+
+
+    @Override
+    public int compare(PCB o1, PCB o2) {
+        if(!(o1 instanceof PCB_FCFS) || !(o2 instanceof PCB_FCFS) )
+            throw new RuntimeException("Objetos não comparaveis");
+
+        int answer = (int) (((PCB_FCFS) o1).idProcesso - ((PCB_SRTF) o2).idProcesso);
+
+        return answer;
+    }
+}
